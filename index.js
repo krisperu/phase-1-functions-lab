59205e1ceaa0ea11fb5cb65d11041756cb25c8f7
Kristina Peru <krissy.germanov@gmail.com>
2022-02-01 11:47:48 -0700

    commit tree 63fbf43e8d97f209989a874fbb3f39dfb78c01c4

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(42 -pickup);
+}
+
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    return Math.abs(num2 - num1) * 264;
+}
+
+function calculatesFarePrice(num1, num2) {
+    const distance = distanceTravelledInFeet(num1, num2);
+    if (distance < 400){
+    return 0;
+    } else if (distance >= 400 && distance <= 2000) {
+        return (distance -400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
